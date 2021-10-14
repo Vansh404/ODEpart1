716ed63c4b2addd9a3b51b71728dc488655ddba2
Vansh404 <79185485+Vansh404@users.noreply.github.com>
2021-10-14 18:11:08 +0530

    Delete ODESolver.py

--- ODESolver.py
+++
@@ -1,9 +0,0 @@
-# this program requires 3 inputs-->1)model-the definition of the function that will give us the derivatives
-#  2)initial conditions
-# 3)time range
-# sample basic eqn=> dy/dt=-ky(t); where y is the aforementioned 'model'
-
-import numpy
-
-
-
